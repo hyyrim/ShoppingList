4108f9b865ff3a06408b6af2f5a76eaedb1c28d3
Hyerim <realD1006@gmail.com>
2021-09-23 22:14:31 +0900

    refactoring
    Event Delegation

--- main copy.js
+++ main copy.js
@@ -1,12 +1,6 @@
 const items = document.querySelector('.items');
-const form = document.querySelector('.new-form');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    onAdd();
-});
 
 function onAdd() {
     // 1. 사용자가 입력한 텍스트 받아옴
@@ -31,46 +25,53 @@ function onAdd() {
     
 }
 
-let id = 0; // UUID
-
 function createItem(text) {
+
+    const li = document.createElement('li');
+    
 
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
-    itemRow.setAttribute('data-id', id);
-    itemRow.innerHTML = `
-            <div class="item">
-                <span class="item__name">${text}</span>
-                <button class="item__delete">
-                    <i class="fas fa-trash-alt" aria-hidden="true" data-id=${id}></i>
-                </button>
-            </div>
-            <div class="item__divider"></div>`;
-    id++;
+
+    const item = document.createElement('div');
+    item.setAttribute('class', 'item');
+
+    const name = document.createElement('span');
+    name.setAttribute('class', 'item__name');
+    name.innerText = text;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'item__delete');
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    deleteBtn.addEventListener('click', ()=> {
+        items.removeChild(itemRow);
+    });
+
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class', 'item__divider');
+
+    item.appendChild(name);
+    item.appendChild(deleteBtn);
+
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
     return itemRow;
 }
 
-/*
 addBtn.addEventListener('click', ()=> {
     onAdd();
 });
 
-input.addEventListener('keydown', (event)=> {
-    if (event.isComposing){
-        return;
-    }
+input.addEventListener('keypress', (event)=> {
     if(event.key === 'Enter'){
         onAdd();
     }
-}); 
-*/
-
-items.addEventListener('click', event => {
-    const id = event.target.dataset.id;
-    if(id) {
-        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`)
-        toBeDeleted.remove();
-    }
 });
 
+const ul = document.querySelector('ul');
+ul.addEventListener('click', event => {
+    if (event.target.targetName == 'LI'){
+        event.target.classList.add('selected');
 
+    }
+});
